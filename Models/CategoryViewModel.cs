7fa4fa45f7ec56f483caabd818ea0c649fa83f1b
Sameer Wadkar <wadkarsamir32@gmail.com>
2025-01-13 15:54:15 +0530

    Added category model, controller and view for different catgeories available in the salon.

---
+++ Models/CategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GlamourZone.Models
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }  // e.g., Full Body Massage, Simple Makeup, etc.
+        public string Description { get; set; }
+        public string PhotosPath { get; set; }
+
+
+    }
+
+}
