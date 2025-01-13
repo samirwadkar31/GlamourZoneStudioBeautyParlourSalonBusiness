8530a83ea3f177f6cad25e8b1de484a0153edf09
Sameer Wadkar <wadkarsamir32@gmail.com>
2025-01-13 16:32:54 +0530

    Performed categories model migrations and made catgories page live by fetching categories details from the catgeories table.

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using GlamourZone.Data;
+using GlamourZone.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GlamourZone.Controllers
 {
     public class CategoriesController : Controller
     {
-        public IActionResult Index()
+        private readonly SalonDbContext _context;
+
+        public CategoriesController(SalonDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View(categories);
+            
         }
     }
 }
