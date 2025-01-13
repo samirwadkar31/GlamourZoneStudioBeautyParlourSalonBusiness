8505247e81be95e1444d6ce9eb104ccc6b37d18a
Sameer Wadkar <wadkarsamir32@gmail.com>
2025-01-13 16:07:55 +0530

    Added salondb context and set up the connection with sql server

---
+++ Data/SalonDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GlamourZone.Models;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System;
+
+namespace GlamourZone.Data
+{
+    public class SalonDbContext : DbContext
+    {
+        public SalonDbContext(DbContextOptions<SalonDbContext> options) : base(options) { }
+
+        public DbSet<CategoryViewModel> Categories { get; set; }
+
+
+    }
+}
